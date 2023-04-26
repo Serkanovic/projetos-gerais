2b5d2db573c66f7eb4b682d819a065b05933ebb1
Júlio R <102775547+Serkanovic@users.noreply.github.com>
2023-04-26 11:50:36 -0300

    changes from php file

---
+++ estudos/php/brco/operators/index.php
@@ -0,0 +1,22 @@
+<?php
+    $age = 25;
+    $citizen = true;
+
+    if($age >= 18 && $citizen){
+        echo "You can vote<br>";
+    }else{
+        echo "You cannot vote<br>";
+    }
+
+    $child = false;
+    $senior = false;
+    $ticket = null;
+
+    if($child || $senior){
+        $ticket = 10;
+    }else{
+        $ticket = "15";
+    }
+
+    echo "The ticket price is \${$ticket}";
+?>
