1a7434147ed6a02aa2b7e2e2ef63a4f5be9ec1e6
Júlio R <102775547+Serkanovic@users.noreply.github.com>
2023-06-26 19:47:31 -0300

    python file updated

---
+++ estudos/python/bsc/num.py
@@ -0,0 +1,11 @@
+x = int(input("Insira um número: "))
+y = int(input("Insira outro número: "))
+
+print(f"""
+A soma entre {x} e {y} é igual {x+y}
+A divisão entre {x} e {y} é igual {x/y}
+A multiplicação entre {x} e {y} é igual {x*y}
+A subtração entre {x} e {y} é igual {x-y}
+A divisão inteira entre {x} e {y} é igual {x//y}
+O resto da divsão entre {x} e {y} é igual {x%y}
+""")
---
+++ estudos/python/test/testpython.py
@@ -0,0 +1,5 @@
+num = int(input("Insira um número para ver sua tabuada: "))
+c = 1
+while c <= 10:
+    print(f"{num} x {c} = {num * c}")
+    c += 1
